60f14f363f1cfca46b8afbcb0bebd19092c930e9
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-03 16:56:01 +0700

    feat: some helper

---
+++ src/helpers/database.rs
@@ -0,0 +1,35 @@
+use std::env;
+use async_once::AsyncOnce;
+use lazy_static::lazy_static;
+use sqlx::postgres::{PgPool, PgPoolOptions};
+
+lazy_static! {
+    pub static ref POOL: AsyncOnce<PgPool> = AsyncOnce::new(async  {
+        let client = open_postgres().await;
+
+        client
+    });
+}
+
+#[doc = "Open connection to postgres database"]
+pub async fn open_postgres() -> PgPool {
+    let url = env::var("DATABASE_URL")
+        .unwrap_or(format!("postgres://postgres:postgres@localhost:5137/postgres"));
+    
+    println!("database connect to {}", url);
+
+    let pool = PgPoolOptions::new()
+        .max_connections(10)
+        .connect(url.as_str())
+        .await
+        .expect("Can't connect to database");
+
+    pool
+}
+
+#[doc = "Connect to postgres database"]
+pub async fn connect_postgres() -> PgPool {
+    let _db = POOL.get().await.to_owned();
+
+    _db
+}
---
+++ src/helpers/parse.rs
@@ -0,0 +1,47 @@
+use serde::Serialize;
+use serde_json::{Value, to_value, Map};
+
+#[doc = "function to convert vector of string to string"]
+pub fn vec_to_string(vec: Vec<&str>) -> String {
+    let mut res = String::new();
+
+    for (i, el) in vec.into_iter().enumerate() {
+        if i != 0 {res.push_str(",")}
+        
+        res.push_str("'");
+        res.push_str(el);
+        res.push_str("'");
+    }
+
+    res
+}
+
+#[doc = "function to get value from map"]
+pub fn map_get(key: &str, data: Value) -> Value {
+    data
+        .as_object()
+        .unwrap_or(&Map::new())
+        .get(key)
+        .unwrap_or(&Value::Null)
+        .to_owned()
+}
+
+#[doc = "function to convert value to string"]
+pub fn to_str(data: Value) -> String {
+    data.as_str().unwrap_or("").to_owned()
+}
+
+#[doc = "function to convert value to i64"]
+pub fn to_i64(data: Value) -> i64 {
+    data.as_i64().unwrap_or(0) as i64
+}
+
+#[doc = "function to convert value to i32"]
+pub fn to_i32(data: Value) -> i32 {
+    data.as_i64().unwrap_or(0) as i32
+}
+
+#[doc = "function to convert any struct to json"]
+pub fn convert_vec_to_values<T: Serialize>(data: Vec<T>) -> Vec<Value> {
+    data.into_iter().map(|item| to_value(item).unwrap()).collect()
+}
