aa572355d25e239f91f92b914f2b114672dd157b
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-03 16:55:53 +0700

    chore: some helper

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -1,3 +1,7 @@
+pub mod database;
+pub mod parse;
 pub mod response;
 
+pub use database::*;
+pub use parse::*;
 pub use response::*;
--- src/helpers/response.rs
+++ src/helpers/response.rs
@@ -1,29 +1,42 @@
 use serde::Serialize;
-use crate::model::Todo;
-
-#[derive(Serialize)]
-pub struct GenericResponse {
-    pub status: String,
-    pub message: String,
-    pub data: String,
-}
+use serde_json::Value;
+use actix_web::{HttpResponse, http::StatusCode};
 
 #[derive(Serialize, Debug)]
-pub struct TodoData {
-    pub todo: Todo,
+pub struct ResponseStruct {
+    status: String,
+    message: String,
+    data: Vec<Value>,
 }
 
-#[derive(Serialize, Debug)]
-pub struct SingleTodoResponse {
-    pub status: String,
-    pub message: String,
-    pub data: TodoData,
-}
+#[doc = "Create a response template"]
+pub fn response_json(status: String, message: String, data: Vec<Value>) -> HttpResponse {
+    // init response
+    let mut code = StatusCode::ACCEPTED;
+    let stats = status.to_owned().to_lowercase();
 
-#[derive(Serialize, Debug)]
-pub struct TodoListResponse {
-    pub status: String,
-    pub message: String,
-    pub results: usize,
-    pub todos: Vec<Todo>,
+    // set status code
+    if stats == "success" {
+        code = StatusCode::OK;
+    } else if stats == "error" {
+        code = StatusCode::INTERNAL_SERVER_ERROR;
+    } else if stats == "failed" {
+        code = StatusCode::BAD_REQUEST;
+    } else if stats == "unauthorize" {
+        code = StatusCode::UNAUTHORIZED;
+    } else if stats == "forbidden" {
+        code = StatusCode::FORBIDDEN;
+    }
+
+    // set response body
+    let body = ResponseStruct {
+        status: status.to_string(),
+        message: message.to_string(),
+        data,
+    };
+
+    // return response
+    HttpResponse::build(code)
+        .content_type("application/json")
+        .json(body)
 }
