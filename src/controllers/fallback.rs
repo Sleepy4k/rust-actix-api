563d6d69b64ee40a3c3d2a626d576bd7a424ef69
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-04 16:43:37 +0700

    chore: fallback controller

--- src/controllers/fallback.rs
+++ src/controllers/fallback.rs
@@ -1,15 +1,22 @@
+use serde_json::json;
 use actix_web::{HttpRequest, Responder};
 
 use crate::helpers::response::*;
 
 #[doc = "Default route for all routes that are not defined"]
 pub async fn fallback(req: HttpRequest) -> impl Responder {
-    let path = req.path();
-    let message = format!("route {} not found", path);
+    let path = req.path().to_string();
+    let method = req.method().to_string();
+    let data = vec![
+        json!({
+            "path": path,
+            "method": method
+        })
+    ];
 
     response_json(
         "not found".to_string(),
-        message,
-        vec![]
+        "Path not found".to_string(),
+        data
     )
 }
