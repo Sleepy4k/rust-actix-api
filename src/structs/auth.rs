87a2f69f75cebfd733748ad3a3cabbc797617b1b
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-05 23:44:40 +0700

    chore: auth struct

--- src/structs/auth.rs
+++ src/structs/auth.rs
@@ -1,7 +1,7 @@
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
 
 #[doc = "Token struct"]
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct TokenStruct {
     pub id: i32,
     pub role: String,
