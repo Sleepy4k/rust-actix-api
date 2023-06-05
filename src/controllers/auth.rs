16e11e44617324499a9b384d348c79407f11e3ba
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-05 23:43:40 +0700

    chore: auth controller

--- src/controllers/auth.rs
+++ src/controllers/auth.rs
@@ -1,4 +1,4 @@
-use sqlx::Error;
+use chrono::Local;
 use serde_json::Value;
 use actix_web::{web::{self}, Responder};
 use bcrypt::{hash, verify, DEFAULT_COST};
@@ -12,6 +12,8 @@ pub async fn login(body: web::Json<Value>) -> impl Responder {
     let username = to_str(map_get("username", body.to_owned()));
     let password = to_str(map_get("password", body.to_owned()));
 
+    let start_time = Local::now().timestamp();
+
     if check_if_empty(username.to_owned()) || check_if_empty(password.to_owned()) {
         return response_json(
             "failed".to_string(),
@@ -21,15 +23,17 @@ pub async fn login(body: web::Json<Value>) -> impl Responder {
     }
 
     let pool = connect_postgres().await;
-    let user = match sqlx::query!("select * from client where username = $1", username)
-        .fetch_one(&pool)
+    let user = match sqlx::query!("select * from client where username = $1 limit 1", username)
+        .fetch_optional(&pool)
         .await {
-            Ok(user) => user,
-            Err(Error::RowNotFound) => return response_json(
-                "failed".to_string(),
-                "Account not found".to_string(),
-                vec![]
-            ),
+            Ok(Some(user)) => user,
+            Ok(None) => {
+                return response_json(
+                    "failed".to_string(),
+                    "Account not found".to_string(),
+                    vec![]
+                );
+            }
             Err(_) => return response_json(
                 "error".to_string(),
                 "Something went wrong".to_string(),
@@ -37,34 +41,38 @@ pub async fn login(body: web::Json<Value>) -> impl Responder {
             )
         };
 
-    let verify_password = verify(password, &user.password).unwrap_or(false);
-
-    if !verify_password {
-        return response_json(
-            "failed".to_string(),
-            "Username or password is wrong".to_string(),
+    match verify(password, &user.password) {
+        Ok(true) => (),
+        Ok(false) => {
+            return response_json(
+                "failed".to_string(),
+                "Username or password is wrong".to_string(),
+                vec![]
+            )
+        }
+        Err(_) => return response_json(
+            "error".to_string(),
+            "Something went wrong".to_string(),
             vec![]
         )
-    }
+    };
+
+    let token_time = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
 
     let token_value = TokenStruct {
         id: user.id,
         username: user.username.clone(),
         role: user.role.clone(),
-        iat: SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_secs(),
-        exp: SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_secs()
-            .saturating_add(60 * 60),
+        iat: token_time,
+        exp: token_time.saturating_add(60 * 60),
     };
     
-    let jwt_secret =  env::var("JWT_SECRET").unwrap_or(String::from("secret"));
+    let jwt_secret = env::var("JWT_SECRET").unwrap_or_else(|_| String::from("secret"));
     let key = EncodingKey::from_secret(jwt_secret.as_ref());
-    let token = encode(&Header::default(), &token_value, &key).unwrap_or(String::from(""));
+    let token = encode(&Header::default(), &token_value, &key).unwrap_or_else(|_| String::new());
 
     let detail_user = convert_vec_to_values(vec![
         DetailUserStruct {
@@ -74,13 +82,18 @@ pub async fn login(body: web::Json<Value>) -> impl Responder {
         }
     ]);
 
+    let end_time = Local::now().timestamp();
+    let duration = modified_duration(start_time, end_time);
+
+    println!("{}", duration);
+
     response_json_with_cookie(
         "success".to_string(),
         "Successfully logged in".to_string(),
         detail_user,
         "set".to_string(),
         "auth_jwt_secret".to_string(),
-        token.clone()
+        token,
     )
 }
 
@@ -89,6 +102,8 @@ pub async fn register(body: web::Json<Value>) -> impl Responder {
     let username = to_str(map_get("username", body.to_owned()));
     let password = to_str(map_get("password", body.to_owned()));
     let role = to_str(map_get("role", body.to_owned()));
+
+    let start_time = Local::now().timestamp();
 
     if check_if_empty(username.to_owned()) || check_if_empty(password.to_owned()) || check_if_empty(role.to_owned()) {
         return response_json(
@@ -99,20 +114,26 @@ pub async fn register(body: web::Json<Value>) -> impl Responder {
     }
 
     let pool = connect_postgres().await;
-    let check_user = sqlx::query!("select id, username from client where username = $1", username.to_owned())
+
+    match sqlx::query!("select id from client where username = $1", username.to_owned())
         .fetch_optional(&pool)
-        .await
-        .unwrap();
+        .await {
+            Ok(Some(_)) => {
+                return response_json(
+                    "failed".to_string(),
+                    "Username already exists".to_string(),
+                    vec![]
+                )
+            },
+            Ok(None) => (),
+            Err(_) => return response_json(
+                "error".to_string(),
+                "Something went wrong".to_string(),
+                vec![]
+            )
+        };
 
-    if check_user.is_some() {
-        return response_json(
-            "failed".to_string(),
-            "Username already exists".to_string(),
-            vec![]
-        )
-    }
-
-    let hashed_password = hash(password, DEFAULT_COST).unwrap_or(String::from(""));
+    let hashed_password = hash(password, DEFAULT_COST).unwrap_or_else(|_| String::new());
 
     match sqlx::query_as!(DetailUserStruct,
         "insert into client (username, password, role) values ($1, $2, $3) returning id, username, role",
@@ -123,6 +144,11 @@ pub async fn register(body: web::Json<Value>) -> impl Responder {
         Ok(data) => {
             let detail_user = convert_vec_to_values(vec![data]);
 
+            let end_time = Local::now().timestamp();
+            let duration = modified_duration(start_time, end_time);
+        
+            println!("{}", duration);
+        
             response_json(
                 "success".to_string(),
                 "Successfully registered".to_string(),
