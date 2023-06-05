1abbf87d4384ec2ff27bf031275e4e4fe5190372
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-05 23:43:50 +0700

    feat: spare part controller

--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -1,9 +1,11 @@
 pub mod auth;
 pub mod fallback;
 pub mod product;
+pub mod spare_part;
 pub mod welcome;
 
 pub use auth::*;
 pub use fallback::*;
 pub use product::*;
+pub use spare_part::*;
 pub use welcome::*;
