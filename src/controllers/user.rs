ae37a6890b120155ec47c031b98d865777983bb8
Sleepy4k <sarahpalastrin@gmail.com>
2023-06-07 16:37:29 +0700

    feat: user controller

---
+++ src/controllers/user.rs
@@ -0,0 +1,20 @@
+use actix_web::Responder;
+
+use crate::{helpers::{response::*, database::connect_postgres, parse::*}, structs::auth::*};
+
+#[doc = "Get all users"]
+pub async fn get_user() -> impl Responder {
+    let pool = connect_postgres().await;
+    let data = sqlx::query_as!(DetailUserStruct, "select id, username, role from client")
+        .fetch_all(&pool)
+        .await
+        .unwrap();
+
+    let result = convert_vec_to_values(data);
+
+    response_json(
+        "success".to_string(),
+        "Successfully retrieved user".to_string(),
+        result
+    )
+}
